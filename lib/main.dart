78fd315cf7390caab9be8d2c080c0505ba2fbfdc
Etang-ken <etangkencliffandocktabiatem@gmail.com>
2024-04-29 01:30:37 +0100

    Transfer branch

--- lib/main.dart
+++ lib/main.dart
@@ -169,7 +169,7 @@ class MyApp extends StatelessWidget {
       height: double.infinity,
       child: Center(
         child: Image.asset(
-          'assets/images/logo/elcrypto.png',
+          'assets/images/logo/eltransfer.png',
           height: 250,
         ),
       ),
